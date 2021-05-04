f0c3a7f73dc7068aaa78609cc1a5b948cdbc4b4e
ice-rahul <meet4g@gmail.com>
2021-05-05 04:31:29 +0530

    minor design fix

--- src/components/OnDemand/index.js
+++ src/components/OnDemand/index.js
@@ -15,12 +15,12 @@ const OnDemand = () => {
       <div className="title"> Most in Demand </div>
       <div className="gallery">
         <div className="gallery-left">
-          <Link to="/product/Men's Clothing" className="cursor-pointer decoration-none"><ImageContainer type={IMAGE_CONTAINER.TYPE.PRIMARY} className="margin-10 cursor-pointer" link={MensClothing} title="Mens Clothing" /></Link>
-          <Link to="/product/Jewelery" className="cursor-pointer decoration-none"><ImageContainer type={IMAGE_CONTAINER.TYPE.PRIMARY} className="margin-10 cursor-pointer" link={Jewellery} title="Jewellery" /></Link>
+          <Link to="/product/Men's Clothing" className="cursor-pointer decoration-none w-100"><ImageContainer type={IMAGE_CONTAINER.TYPE.PRIMARY} className="margin-10 cursor-pointer" link={MensClothing} title="Mens Clothing" /></Link>
+          <Link to="/product/Jewelery" className="cursor-pointer decoration-none w-100"><ImageContainer type={IMAGE_CONTAINER.TYPE.PRIMARY} className="margin-10 cursor-pointer" link={Jewellery} title="Jewellery" /></Link>
         </div>
         <div className="gallery-right">
-          <Link to="/product/Women's Clothing" className="cursor-pointer decoration-none"><ImageContainer type={IMAGE_CONTAINER.TYPE.SECONDARY} className="margin-10 cursor-pointer" link={WomensClothing} title="Womens Clothing" /></Link>
-          <Link to="/product/Electronics" className="cursor-pointer decoration-none"><ImageContainer type={IMAGE_CONTAINER.TYPE.SECONDARY} className="margin-10 cursor-pointer" link={ElectronicGadgets} title="Electronic Gadgets" /></Link>
+          <Link to="/product/Women's Clothing" className="cursor-pointer decoration-none w-100"><ImageContainer type={IMAGE_CONTAINER.TYPE.SECONDARY} className="margin-10 cursor-pointer" link={WomensClothing} title="Womens Clothing" /></Link>
+          <Link to="/product/Electronics" className="cursor-pointer decoration-none w-100"><ImageContainer type={IMAGE_CONTAINER.TYPE.SECONDARY} className="margin-10 cursor-pointer" link={ElectronicGadgets} title="Electronic Gadgets" /></Link>
         </div>
       </div>
     </section>
---
+++ src/pages/Delivery.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Delivery = () => {
+  return (
+    <>
+      Delivery
+    </>
+  );
+}
+
+export default Delivery;
